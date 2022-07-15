a4ea9a81adadc823e359ef8bbad7fd68f9986658
V4anderson <55470760+V4anderson@users.noreply.github.com>
2022-07-15 01:20:53 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace CrudSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbID = new System.Windows.Forms.TextBox();
+            this.LblID = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.tbNome = new System.Windows.Forms.TextBox();
+            this.lblNumero = new System.Windows.Forms.Label();
+            this.tbNumero = new System.Windows.Forms.TextBox();
+            this.btnNovo = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.btnExibir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbID
+            // 
+            this.tbID.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.tbID.Location = new System.Drawing.Point(96, 22);
+            this.tbID.Name = "tbID";
+            this.tbID.Size = new System.Drawing.Size(100, 23);
+            this.tbID.TabIndex = 0;
+            // 
+            // LblID
+            // 
+            this.LblID.AutoSize = true;
+            this.LblID.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.LblID.Location = new System.Drawing.Point(16, 22);
+            this.LblID.Name = "LblID";
+            this.LblID.Size = new System.Drawing.Size(25, 17);
+            this.LblID.TabIndex = 1;
+            this.LblID.Text = "ID:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 195);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(524, 150);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblNome.Location = new System.Drawing.Point(16, 51);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(49, 17);
+            this.lblNome.TabIndex = 4;
+            this.lblNome.Text = "Nome:";
+            // 
+            // tbNome
+            // 
+            this.tbNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.tbNome.Location = new System.Drawing.Point(96, 51);
+            this.tbNome.Name = "tbNome";
+            this.tbNome.Size = new System.Drawing.Size(166, 23);
+            this.tbNome.TabIndex = 3;
+            // 
+            // lblNumero
+            // 
+            this.lblNumero.AutoSize = true;
+            this.lblNumero.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblNumero.Location = new System.Drawing.Point(16, 80);
+            this.lblNumero.Name = "lblNumero";
+            this.lblNumero.Size = new System.Drawing.Size(62, 17);
+            this.lblNumero.TabIndex = 6;
+            this.lblNumero.Text = "Número:";
+            // 
+            // tbNumero
+            // 
+            this.tbNumero.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.tbNumero.Location = new System.Drawing.Point(96, 80);
+            this.tbNumero.Name = "tbNumero";
+            this.tbNumero.Size = new System.Drawing.Size(166, 23);
+            this.tbNumero.TabIndex = 5;
+            // 
+            // btnNovo
+            // 
+            this.btnNovo.Location = new System.Drawing.Point(12, 166);
+            this.btnNovo.Name = "btnNovo";
+            this.btnNovo.Size = new System.Drawing.Size(100, 23);
+            this.btnNovo.TabIndex = 7;
+            this.btnNovo.Text = "Novo";
+            this.btnNovo.UseVisualStyleBackColor = true;
+            this.btnNovo.Click += new System.EventHandler(this.btnNovo_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(118, 166);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(100, 23);
+            this.btnEditar.TabIndex = 8;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(224, 166);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(100, 23);
+            this.btnExcluir.TabIndex = 9;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Location = new System.Drawing.Point(330, 166);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(100, 23);
+            this.btnConsultar.TabIndex = 10;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
+            // 
+            // btnExibir
+            // 
+            this.btnExibir.Location = new System.Drawing.Point(436, 166);
+            this.btnExibir.Name = "btnExibir";
+            this.btnExibir.Size = new System.Drawing.Size(100, 23);
+            this.btnExibir.TabIndex = 11;
+            this.btnExibir.Text = "Exibir";
+            this.btnExibir.UseVisualStyleBackColor = true;
+            this.btnExibir.Click += new System.EventHandler(this.btnExibir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(546, 365);
+            this.Controls.Add(this.btnExibir);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnNovo);
+            this.Controls.Add(this.lblNumero);
+            this.Controls.Add(this.tbNumero);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.tbNome);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.LblID);
+            this.Controls.Add(this.tbID);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbID;
+        private System.Windows.Forms.Label LblID;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.TextBox tbNome;
+        private System.Windows.Forms.Label lblNumero;
+        private System.Windows.Forms.TextBox tbNumero;
+        private System.Windows.Forms.Button btnNovo;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.Button btnExibir;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace CrudSystem
+{
+    public partial class Form1 : Form
+    {
+
+        MySqlConnection conexao;
+        MySqlCommand comando;
+        MySqlDataAdapter da;
+        MySqlDataReader dr;
+        string strSQL;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNovo_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conexao = new MySqlConnection("Server=localhost;Database=base;Uid=root;Pwd=;");
+
+                strSQL = "INSERT INTO cad_clientes (nome, numero) values (@nome, @numero)";
+
+                comando = new MySqlCommand(strSQL, conexao);
+
+                comando.Parameters.AddWithValue("@nome", tbNome.Text);
+
+                comando.Parameters.AddWithValue("@numero", tbNumero.Text);
+
+                conexao.Open();
+
+                comando.ExecuteNonQuery();
+
+                MessageBox.Show("Cadastro realizado com Sucesso!");
+
+
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+
+                conexao = null;
+
+                comando = null;
+
+
+            }
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conexao = new MySqlConnection("Server=localhost;Database=base;Uid=root;Pwd=;");
+
+                strSQL = "UPDATE cad_clientes set nome = @nome, numero = @numero where id = @id";
+
+                comando = new MySqlCommand(strSQL, conexao);
+
+                comando.Parameters.AddWithValue("@id", tbID.Text);
+
+                comando.Parameters.AddWithValue("@nome", tbNome.Text);
+
+                comando.Parameters.AddWithValue("@numero", tbNumero.Text);
+
+                conexao.Open();
+
+                comando.ExecuteNonQuery();
+
+                MessageBox.Show("Atualização realizada com Sucesso!");
+
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+
+                conexao = null;
+
+                comando = null;
+
+
+            }
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conexao = new MySqlConnection("Server=localhost;Database=base;Uid=root;Pwd=;");
+
+                strSQL = "DELETE from cad_clientes where id = @id";
+
+                comando = new MySqlCommand(strSQL, conexao);
+
+                comando.Parameters.AddWithValue("@id", tbID.Text);
+
+                conexao.Open();
+
+                comando.ExecuteNonQuery();
+
+                MessageBox.Show("Apagado com Sucesso!");
+
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+
+                conexao = null;
+
+                comando = null;
+
+
+            }
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conexao = new MySqlConnection("Server=localhost;Database=base;Uid=root;Pwd=;");
+
+                strSQL = "SELECT * FROM cad_clientes where id = @id";
+
+                comando = new MySqlCommand(strSQL, conexao);
+
+                comando.Parameters.AddWithValue("@id", tbID.Text);
+
+                conexao.Open();
+
+                dr = comando.ExecuteReader();
+
+                while(dr.Read())
+                {
+                    tbNome.Text = Convert.ToString(dr["nome"]);
+                    tbNumero.Text = Convert.ToString(dr["numero"]);
+                }
+
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+
+                conexao = null;
+
+                comando = null;
+
+
+            }
+        }
+
+        private void btnExibir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conexao = new MySqlConnection("Server=localhost;Database=base;Uid=root;Pwd=;");
+
+                strSQL = "SELECT * FROM cad_clientes";
+
+                da = new MySqlDataAdapter(strSQL, conexao);
+
+                DataTable dt = new DataTable();
+
+                da.Fill(dt);
+
+                dataGridView1.DataSource = dt;
+
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+
+                conexao = null;
+
+                comando = null;
+
+
+            }
+        }
+    }
+}
